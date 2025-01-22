aa78a5914f9a5999195c65cdaa608ac2ca94ff50
Joshua <97928637+jgx02c@users.noreply.github.com>
2025-01-22 14:35:34 -0800

    Update Python-Notes.py

---
+++ Python/Python-Notes.py
@@ -0,0 +1,37 @@
+# Python Notes
+
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number. 
+
+# You can get the data type of any object by using the type() function.
+
+# insert method
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+
+# remove method
+thislist.remove("banana")
+
+# the pop method removes the specified index
+thislist.remove("banana")
+
+# delete can be used to delete an index or the entire list
+del thislist[0]
+del thislist
+
+# clear the list but it still remains, just empty
+thislist.clear()
+
+# looping throught a list
+for x in thislist:
+  print(x)
+
+
+
+
+#Loop throught the index numbers
+# You can also loop through the list items by referring to their index number.
+# Use the range() and len() functions to create a suitable iterable.
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
