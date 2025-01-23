c080d8b89dccb63658369e1dbdbeb4d36befc22a
Joshua <97928637+jgx02c@users.noreply.github.com>
2025-01-22 16:27:54 -0800

    Update Python-Notes.py

--- Python/Python-Notes.py
+++ Python/Python-Notes.py
@@ -26,12 +26,23 @@ thislist.clear()
 for x in thislist:
   print(x)
 
-
-
-
-#Loop throught the index numbers
+# Loop throught the index numbers
 # You can also loop through the list items by referring to their index number.
 # Use the range() and len() functions to create a suitable iterable.
 thislist = ["apple", "banana", "cherry"]
 for i in range(len(thislist)):
   print(thislist[i])
+
+
+# Using a While Loop
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+# .sort() will sort alphabetically or numberically
+thislist.sort()
+
+# To sort descending, use the keyword argument reverse = True:
+thislist.sort(reverse = True)
